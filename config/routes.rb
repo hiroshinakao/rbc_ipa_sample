574ac19441d6b1d3d38e655712e990bbe964905c
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2010-09-18 16:24:16 +0900

    rails g devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Twitter::Application.routes.draw do
+  devise_for :users
+
   resources :tweets
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100918055753) do
+ActiveRecord::Schema.define(:version => 20100918072011) do
 
   create_table "tweets", :force => true do |t|
     t.string   "tweet"
@@ -19,17 +19,22 @@ ActiveRecord::Schema.define(:version => 20100918055753) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "login",                     :limit => 40
-    t.string   "name",                      :limit => 100, :default => ""
-    t.string   "email",                     :limit => 100
-    t.string   "crypted_password",          :limit => 40
-    t.string   "salt",                      :limit => 40
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "remember_token",            :limit => 40
-    t.datetime "remember_token_expires_at"
   end
 
-  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
