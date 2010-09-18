156434291ae8d76b0baee5e0a3cb1f542f419b67
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2010-09-18 14:28:26 +0900

    crate rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '2e98d97d8058f211f6956aa87069100d389422c6a6ac95d010358d5a364fdec8b0d093e5d4e38cefd3876359a8bfb44cf176a3eba0909802b70b1cb65777b2fc'
